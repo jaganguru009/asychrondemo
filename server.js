18f81bf3d7ee203f378cbbfb592957a715e99103
jj <jaganguru009@gmail.com>
2021-01-10 15:47:30 +0530

    dasd

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const validate = require("./middlewares/requestValidator").validate;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const http = require("http").Server(app); 
 try {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
