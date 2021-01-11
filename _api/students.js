d86d34787bb4b0d4925f7459258258cb11e75495
jj <jaganguru009@gmail.com>
2021-01-11 14:15:31 +0530

    parent api

--- _api/students.js
+++ _api/students.js
@@ -7,36 +7,59 @@ const emitter = new events.EventEmitter();
 
 
 router.get("/", (req, res) => {
-  Student.find({}, (err, msgs) => {
-    if (err) {
-      res.sendStatus(500);
-    }
-    res.send(msgs);
-  });
+  let parentName = req.query.parentName;
+  console.log("req.query.parentName " +req.query.parentName)
+  if (req.query.parentName != undefined) {
+    Student.find({
+      $and: [
+        { $and: [{ middleName: parentName }] }
+      ]
+    }, function (err, results) {
+      if (err) {
+        console.log("error occured while searching stduents for parent ");
+        res.sendStatus(500);
+      }
+      else {
+        res.send(results);
+      }
+      // if (results.length == 0) {
+      //     return false;
+      // } else {
+      //     return true;
+      // }
+    });
+  } else {
+    Student.find({}, (err, msgs) => {
+      if (err) {
+        res.sendStatus(500);
+      }
+      res.send(msgs);
+    });
+
+  }
 });
+
 router.post("/", (req, res) => {
   console.log(`body   ${JSON.stringify(req.body)}`);
-  var students=req.body;
-  for(let i =0;i<students.length;i++)
-  {
+  var students = req.body;
+  for (let i = 0; i < students.length; i++) {
     var student = new Student(students[i]);
     student
-    .save()
-    .then(() => {
-      console.log("called post"); 
-      if(i==(students.length -1))
-      {
-       res.send(req.body);
-       return;
-      }
-    })
-    .catch((err) => {
-      res.send(err.message);
-      return;
-    });
-  
-  } 
-  
+      .save()
+      .then(() => {
+        console.log("called post");
+        if (i == (students.length - 1)) {
+          res.send(req.body);
+          return;
+        }
+      })
+      .catch((err) => {
+        res.send(err.message);
+        return;
+      });
+
+  }
+
 });
 
 module.exports = router;
